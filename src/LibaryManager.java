147b0a4ed2ef8d3b0992122c54be459d97b7af69
23DP4MMisk <A230427MM@rvt.lv>
2025-03-04 09:56:50 +0200

    hghg

--- src/LibaryManager.java
+++ src/LibaryManager.java
@@ -1,5 +1,6 @@
 public class LibaryManager {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("dfdfd");
     }
 }
