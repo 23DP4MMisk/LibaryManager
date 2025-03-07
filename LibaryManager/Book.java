a5d84aaeb4bc5cb3de476b4a3f3e592ad31f3942
23DP4MMisk <A230427MM@rvt.lv>
2025-03-07 13:30:51 +0200

    jaunas palig klases

---
+++ LibaryManager/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    private String name;
+    private String author;
+    private int year;
+    private boolean busy;
+
+    public Book(String InitilazationName, String InitilazationAuthor, int Initilazationyear){
+        this.name = InitilazationName;
+        this.author = InitilazationAuthor;
+        this.year = Initilazationyear;
+        this.busy = false;
+    }
+
+    
+}
--- LibaryManager/LibaryManager.java
+++ LibaryManager/LibaryManager.java
@@ -1,5 +1,9 @@
+
+import java.util.*;
+
+
 public class LibaryManager {
     public static void main(String[] args) throws Exception {
-       
+       Scanner scanner = new Scanner(System.in);
     }
 }
