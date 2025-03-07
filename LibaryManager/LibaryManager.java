6f016f5648d5e207a701a663c7fb698583cf07ec
23DP4MMisk <A230427MM@rvt.lv>
2025-03-07 12:37:11 +0200

    pievienoju paligas klasus

--- LibaryManager/LibaryManager.java
+++ LibaryManager/LibaryManager.java
@@ -1,6 +1,5 @@
 public class LibaryManager {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("dfdfd");
+       
     }
 }
