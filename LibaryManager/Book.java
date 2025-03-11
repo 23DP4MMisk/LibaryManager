cb38e6526852d11fd9778f0679f3d6ecac02767a
23DP4MMisk <A230427MM@rvt.lv>
2025-03-11 10:38:07 +0200

    Book klas un viņu testešana libaryManager

--- LibaryManager/Book.java
+++ LibaryManager/Book.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Book {
     private String name;
     private String author;
@@ -9,6 +11,69 @@ public class Book {
         this.author = InitilazationAuthor;
         this.year = Initilazationyear;
         this.busy = false;
+    }
+
+    public String bookName(){
+        return this.name;
+    }
+
+    public String authorName(){
+        return this.author;
+    }
+
+    public int bookYear(){
+        return this.year;
+    }
+
+    public boolean isBorrowed(){
+        return this.busy;
+    }
+
+    public void borrow(){
+        this.busy = true;
+    }
+
+    public void returnBook(){
+        this.busy = false;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Title: %s, Author: %s, Year: %d, Borrowed: %b", this.name, this.author, this.year, this.busy);
+    }
+
+    public static void addBook(List<Book> books, String InitilazationName, String InitilazationAuthor, int Initilazationyear){
+        books.add(new Book(InitilazationName, InitilazationAuthor, Initilazationyear));
+    }
+
+    public static void removeBook(List<Book> books, String InitilazationName){
+        books.removeIf(book -> book.bookName().equalsIgnoreCase(InitilazationName));
+    }
+
+    public static Book findBook(List<Book> books, String InitilazationName){
+        return books.stream()
+        .filter(book -> book.bookName().equalsIgnoreCase(InitilazationName))
+        .findFirst()
+        .orElse(null);
+
+    }
+
+    public static void listBooks(List<Book> books){
+        System.out.println("\n==============================================================");
+        System.out.println("       Author     |       Book name           |     Book year ");
+        System.out.println("===============================================================");
+        books.forEach(book -> {
+            System.out.printf("%-20s | %-30s | %-4d%n", book.authorName(), book.bookName(), book.bookYear());
+        });
+    }
+
+    public static void countBooks(List<Book> books){
+        System.out.println("Vispar gramatu biblioteka: " + books.size());
+    }
+
+    public static void countBorrowedBook(List<Book>books){
+        long count = books.stream().filter(Book::isBorrowed).count();
+        System.out.println("Gramatas aizņemtas skaits: " + count);
     }
 
     
